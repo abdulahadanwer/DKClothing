30061926cda3ec77e9d81265d27657d9c9293d5b
Abdul Ahad Anwer Khan <you@example.com>
2018-01-25 22:11:23 -0500

    DK Clothing

---
+++ DKClothing/src/main/java/com/dk/clothing/DkClothingApplication.java
@@ -0,0 +1,12 @@
+package com.dk.clothing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DkClothingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DkClothingApplication.class, args);
+	}
+}
